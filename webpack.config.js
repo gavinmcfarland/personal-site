b1ff38e1f176a9db682beb9e23b095d47a0d4b86
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2019-09-21 14:58:49 +0100

    trying to create PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 const path = require("path");
 const webpack = require("webpack");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const {
+  CleanWebpackPlugin
+} = require("clean-webpack-plugin");
 const MarkoPlugin = require("@marko/webpack/plugin").default;
 const CSSExtractPlugin = require("mini-css-extract-plugin");
 const SpawnServerPlugin = require("spawn-server-webpack-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-const { NODE_ENV } = process.env;
+const {
+  NODE_ENV
+} = process.env;
 const isProd = NODE_ENV === "production";
 const isDev = !isProd;
 const markoPlugin = new MarkoPlugin();
 const spawnedServer = isDev && new SpawnServerPlugin();
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = [
   compiler({
@@ -30,14 +36,21 @@ module.exports = [
       stats: "minimal",
       contentBase: false,
       ...spawnedServer.devServerConfig
-    }: undefined,
+    } : undefined,
     plugins: [
       new webpack.DefinePlugin({
-        "process.browser": true
+        "process.browser": true,
       }),
       new CSSExtractPlugin({
         filename: "[name].[contenthash:8].css"
       }),
+
+      // new PrerenderSPAPlugin({
+      //   // Required - The path to the webpack-outputted app to prerender.
+      //   staticDir: path.join(__dirname, "dist/client"),
+      //   // Required - Routes to render.
+      //   routes: ['/'],
+      // }),
       isProd && new OptimizeCssAssetsPlugin(),
       markoPlugin.browser
     ]
@@ -54,9 +67,12 @@ module.exports = [
       path: path.join(__dirname, "dist/server")
     },
     plugins: [
+      // Trying to pass variables that the template needs. Where do I get $__MARKO_MANIFEST__$ from?
       new webpack.DefinePlugin({
         "process.browser": undefined,
-        "process.env.BUNDLE": true
+        "process.env.BUNDLE": true,
+        'process.env': JSON.stringify(process.env),
+        '$__MARKO_MANIFEST__$': true
       }),
       new webpack.BannerPlugin({
         banner: 'require("source-map-support").install();',
@@ -65,6 +81,19 @@ module.exports = [
       new CSSExtractPlugin({
         filename: "[name].[contenthash:8].css"
       }),
+      // Can't get variables to be available to the template
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: path.resolve(__dirname, 'src/pages/home/index.js'),
+        filename: path.resolve(__dirname, 'dist/index.html')
+      }),
+      // new HtmlWebpackPlugin(),
+      // new PrerenderSPAPlugin({
+      //   // Required - The path to the webpack-outputted app to prerender.
+      //   staticDir: path.join(__dirname, "dist/client"),
+      //   // Required - Routes to render.
+      //   routes: ['/'],
+      // }),
       isDev && spawnedServer,
       markoPlugin.server
     ]
@@ -85,8 +114,7 @@ function compiler(config) {
       extensions: [".js", ".json", ".marko"]
     },
     module: {
-      rules: [
-        {
+      rules: [{
           test: /\.marko$/,
           loader: "@marko/webpack/loader"
         },
