8d58a4572f4e19483dc0a8a0454f8a17f4a84c11
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2019-09-23 00:56:03 +0100

    work in progress

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    plugins: {
-        'postcss-import': {},
-        'postcss-nested': {},
-        'postcss-extend-rule': {},
-        'autoprefixer': {},
-        'tailwindcss': {}
-    }
+	plugins: {
+		'postcss-import': {},
+		'tailwindcss': {},
+		'postcss-preset-env': {},
+		'postcss-nested': {},
+		'postcss-extend-rule': {},
+		'autoprefixer': {}
+	}
 }
